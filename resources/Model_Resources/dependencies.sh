c4cf451956cae9c506fd5f55b2359a72955a1380
Alexandros Paterakis <paterakis.alexandros2000@gmail.com>
2024-06-07 19:15:18 +0300

    uploaded notebooks for model conversions

---
+++ resources/Model_Resources/dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pip3 install numpy urllib opencv-python matplotlib adversarial-robustness-toolbox
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+
